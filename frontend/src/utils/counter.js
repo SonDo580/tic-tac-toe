f3b76f8964e9c5af596d9d5dd2eded17524f724b
Son Do <dominhson580@gmail.com>
2023-10-06 22:33:16 +0700

    Create function to count mark after each move

---
+++ frontend/src/utils/counter.js
@@ -0,0 +1,106 @@
+import { SIDE } from "@/constants";
+
+const countHorizontal = (board, row, column, mark) => {
+  let markNumber = 1;
+  let currentColumn = column;
+
+  while (--currentColumn >= 0) {
+    if (board[row][currentColumn] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  currentColumn = column;
+  while (++currentColumn < SIDE) {
+    if (board[row][currentColumn] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  return markNumber;
+};
+
+const countVertical = (board, row, column, mark) => {
+  let markNumber = 1;
+  let currentRow = row;
+
+  while (--currentRow >= 0) {
+    if (board[currentRow][column] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  currentRow = row;
+  while (++currentRow < SIDE) {
+    if (board[currentRow][column] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  return markNumber;
+};
+
+const countNorthWest_SouthEast = (board, row, column, mark) => {
+  let markNumber = 1;
+  let currentRow = row;
+  let currentColumn = column;
+
+  while (--currentRow >= 0 && --currentColumn >= 0) {
+    if (board[currentRow][currentColumn] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  currentRow = row;
+  currentColumn = column;
+  while (++currentRow < SIDE && ++currentColumn < SIDE) {
+    if (board[currentRow][currentColumn] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  return markNumber;
+};
+
+const countNorthEast_SouthWest = (board, row, column, mark) => {
+  let markNumber = 1;
+  let currentRow = row;
+  let currentColumn = column;
+
+  while (--currentRow >= 0 && ++currentColumn < SIDE) {
+    if (board[currentRow][currentColumn] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  currentRow = row;
+  currentColumn = column;
+  while (++currentRow < SIDE && --currentColumn >= 0) {
+    if (board[currentRow][currentColumn] !== mark) {
+      break;
+    }
+    markNumber++;
+  }
+
+  return markNumber;
+};
+
+const countAllDirections = (...args) => {
+  const markCount = {
+    horizontal: countHorizontal(...args),
+    vertical: countVertical(...args),
+    northwest_southeast: countNorthWest_SouthEast(...args),
+    northeast_southwest: countNorthEast_SouthWest(...args),
+  };
+
+  return markCount;
+};
+
+export { countAllDirections };
