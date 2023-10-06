d4236a5fa63b429af8fb1b7c033b3f23cd0aa306
Son Do <dominhson580@gmail.com>
2023-10-06 23:19:59 +0700

    Highlight all winning lines

--- frontend/src/components/Board/index.jsx
+++ frontend/src/components/Board/index.jsx
@@ -3,6 +3,7 @@ import { produce } from "immer";
 
 import { MarkContext } from "@/context/MarkContext";
 import { SIDE } from "@/constants";
+import { getHighlightCells } from "@/utils/highlighter";
 import Cell from "@/components/Cell";
 
 const createBoard = () => {
@@ -17,16 +18,25 @@ const createBoard = () => {
 export default function Board() {
   const [board, setBoard] = useState(createBoard);
   const { mark, switchMark } = useContext(MarkContext);
+  const [highlightCells, setHighlightCells] = useState([]);
 
   const updateBoard = (rowIndex, colIndex) => {
     setBoard((prevBoard) =>
       produce(prevBoard, (draft) => {
         draft[rowIndex][colIndex] = mark;
+
+        const cells = getHighlightCells(draft, rowIndex, colIndex, mark);
+        if (cells.length > 0) {
+          setHighlightCells(cells);
+        }
       })
     );
 
     switchMark();
   };
+
+  const shouldHighlight = (rowIndex, colIndex) =>
+    highlightCells.includes(`${rowIndex}-${colIndex}`);
 
   return (
     <div className="board">
@@ -38,6 +48,7 @@ export default function Board() {
               mark={mark}
               row={i}
               col={j}
+              highlight={shouldHighlight(i, j)}
               handleClick={updateBoard}
             />
           ))}
