e1b898ee817eaad3ad517f40bf7b66aaf69acbdf
Son Do <dominhson580@gmail.com>
2023-10-06 22:05:00 +0700

    Style the Cell

--- frontend/src/components/Cell/index.jsx
+++ frontend/src/components/Cell/index.jsx
@@ -1,10 +1,18 @@
 import PropTypes from "prop-types";
+import { MARK } from "@/constants";
+
+const COLOR_CLASS = {
+  [MARK.X]: "x",
+  [MARK.O]: "o",
+};
+
+const getColorClass = (mark) => COLOR_CLASS[mark] || "";
 
 export default function Cell({ mark, row, col, handleClick }) {
   const selectCell = () => handleClick(row, col);
 
   return (
-    <div className="cell" onClick={selectCell}>
+    <div className={`cell ${getColorClass(mark)}`} onClick={selectCell}>
       {mark}
     </div>
   );
