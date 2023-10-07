3cb8b14bcdbd998bf8a948723c9858f15530b9b4
Son Do <dominhson580@gmail.com>
2023-10-07 11:43:48 +0700

    Use the Board context

--- frontend/src/components/Board/index.jsx
+++ frontend/src/components/Board/index.jsx
@@ -1,19 +1,10 @@
 import { Fragment, useContext, useState } from "react";
 import { produce } from "immer";
 
-import { SIDE } from "@/constants";
 import { MarkContext } from "@/context/MarkContext";
+import { BoardContext } from "@/context/BoardContext";
 import { getHighlightCells } from "@/utils/highlighter";
 import Cell from "@/components/Cell";
-
-const createBoard = () => {
-  const board = [];
-  for (let i = 0; i < SIDE; i++) {
-    const row = Array(SIDE).fill("");
-    board.push(row);
-  }
-  return board;
-};
 
 const getUpdatedBoard = (board, rowIndex, colIndex, mark) =>
   produce(board, (draft) => {
@@ -21,7 +12,7 @@ const getUpdatedBoard = (board, rowIndex, colIndex, mark) =>
   });
 
 export default function Board() {
-  const [board, setBoard] = useState(createBoard);
+  const { board, setBoard } = useContext(BoardContext);
   const { mark, switchMark } = useContext(MarkContext);
   const [highlightCells, setHighlightCells] = useState([]);
 
@@ -34,7 +25,13 @@ export default function Board() {
 
         draft[rowIndex][colIndex] = mark;
 
-        const updatedBoard = getUpdatedBoard(board, rowIndex, colIndex, mark);
+        const updatedBoard = getUpdatedBoard(
+          prevBoard,
+          rowIndex,
+          colIndex,
+          mark
+        );
+
         const cells = getHighlightCells(updatedBoard, rowIndex, colIndex, mark);
         if (cells.length > 0) {
           setHighlightCells(cells);
