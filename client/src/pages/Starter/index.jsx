2f17202ab88c8ada070ee6105ca32f70fdc733e7
Son Do <dominhson580@gmail.com>
2023-10-09 17:41:41 +0700

    Create Starter page

---
+++ client/src/pages/Starter/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Starter() {
+  return (
+    <div>
+      <h1>Welcome to Tic-Tac-Toe</h1>
+      <Link to="create">Create room</Link>
+      <Link to="join">Join a room</Link>
+      <Link to="guide">How to play</Link>
+    </div>
+  );
+}
