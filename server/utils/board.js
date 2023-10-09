122926bc352bd765e2f05895e50189e68559b73d
Son Do <dominhson580@gmail.com>
2023-10-09 22:28:17 +0700

    Define createBoard function

---
+++ server/utils/board.js
@@ -0,0 +1,12 @@
+import { SIDE } from "../constants";
+
+const createBoard = () => {
+  const board = [];
+  for (let i = 0; i < SIDE; i++) {
+    const row = Array(SIDE).fill("");
+    board.push(row);
+  }
+  return board;
+};
+
+export { createBoard };
