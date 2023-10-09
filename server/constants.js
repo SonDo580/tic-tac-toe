2c4b9f0321b7c1d5dc00f77f40af2a363c1010f5
Son Do <dominhson580@gmail.com>
2023-10-09 20:54:01 +0700

    Add constants

---
+++ server/constants.js
@@ -0,0 +1,16 @@
+const SIDE = 16;
+const WIN_COUNT = 5;
+
+const MARK = {
+  X: "X",
+  O: "O",
+};
+
+const DIRECTION = {
+  horizontal: "horizontal",
+  vertical: "vertical",
+  northwest_southeast: "northwest_southeast",
+  northeast_southwest: "northeast_southwest",
+};
+
+export { MARK, SIDE, DIRECTION, WIN_COUNT };
