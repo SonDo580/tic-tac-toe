07b5ab67545e96322d36d21bf0536d773134e0dd
Son Do <dominhson580@gmail.com>
2023-10-09 22:37:18 +0700

    Handle creating room

--- server/socket.js
+++ server/socket.js
@@ -1,25 +1,24 @@
-const { Server } = require("socket.io");
+import { Server } from "socket.io";
+import { createRoom } from "./utils/room.js";
+
+const rooms = [];
 
 const allowOrigins = ["http://localhost:5173"]; // add live client URL later
 
 const runSocketIO = (httpServer) => {
   const io = new Server(httpServer, {
-    cors: {
-      origin: allowOrigins,
-    },
+    cors: { origin: allowOrigins },
   });
 
   io.on("connection", (socket) => {
     const playerId = socket.id;
 
     socket.on("createRoom", (playerName) => {
-      // const roomId = playerId;
-      // const players = [playerName];
-      // const board = createBoard();
-
-      console.log(playerName);
+      const room = createRoom(playerId, playerName);
+      rooms.push(room);
+      socket.emit("roomCreated", room);
     });
   });
 };
 
-module.exports = runSocketIO;
+export default runSocketIO;
