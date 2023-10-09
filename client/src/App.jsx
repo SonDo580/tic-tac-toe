98cf82c9ff9f4bd835defcfb10edb7ce717d6c81
Son Do <dominhson580@gmail.com>
2023-10-09 17:41:08 +0700

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Starter from "@/pages/Starter";
 import Game from "@/pages/Game";
+import CreateRoom from "@/pages/CreateRoom";
+import JoinRoom from "@/pages/JoinRoom";
+import Guide from "@/pages/Guide";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Starter />} />
+        <Route path="/create" element={<CreateRoom />} />
+        <Route path="/join" element={<JoinRoom />} />
+        <Route path="/guide" element={<Guide />} />
         <Route path="/game" element={<Game />} />
       </Routes>
     </BrowserRouter>
