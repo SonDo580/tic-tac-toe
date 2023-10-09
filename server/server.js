cc9a791886c914575b5f507e1e6c69ddcbf9cc89
Son Do <dominhson580@gmail.com>
2023-10-09 10:16:21 +0700

    Config CORS

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const http = require("node:http");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+// CORS Config
+const allowedOrigins = ["http://localhost:5173"]; // add live url later
+const corsOptions = {
+  origin: allowedOrigins,
+};
+app.use(cors(corsOptions));
 
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
