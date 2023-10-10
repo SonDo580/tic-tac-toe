c6d157d16a3468da305e1143be1ee1f0e6f86b4c
Son Do <dominhson580@gmail.com>
2023-10-10 14:44:52 +0700

    Create JoinRoom form

---
+++ client/src/pages/JoinRoom/index.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { socket } from "@/utils/socket";
+
+const nameErrorMessage =
+  "Name is required and must contains non-space characters";
+
+const roomIdEmptyMessage = "Room ID is required";
+
+export default function JoinRoom() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [nameError, setNameError] = useState("");
+  const changeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const [roomID, setRoomID] = useState("");
+  const [roomError, setRoomError] = useState("");
+  const changeRoomID = (event) => {
+    setRoomID(event.target.value);
+  };
+
+  const formHasError = () => {
+    let hasError = false;
+    if (name.trim() === "") {
+      setNameError(nameErrorMessage);
+      hasError = true;
+    }
+    if (roomID === "") {
+      setRoomError(roomIdEmptyMessage);
+      hasError = true;
+    }
+    return hasError;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formHasError()) {
+      return;
+    }
+    setNameError("");
+    setRoomError("");
+    socket.emit("joinRoom", { playerName: name, roomID });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={changeName}
+      />
+      <span>{nameError}</span>
+
+      <input
+        type="text"
+        placeholder="Enter room ID"
+        value={roomID}
+        onChange={changeRoomID}
+      />
+      <span>{roomError}</span>
+
+      <button>Join</button>
+    </form>
+  );
+}
