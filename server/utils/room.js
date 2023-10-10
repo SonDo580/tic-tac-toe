0d1c6417646093af0b9c0646129b1724a21dc321
Son Do <dominhson580@gmail.com>
2023-10-10 15:18:12 +0700

    Add function searchRoom

--- server/utils/room.js
+++ server/utils/room.js
@@ -1,6 +1,8 @@
 import { v4 as uuid } from "uuid";
 import { createBoard } from "./board.js";
 
+const rooms = [];
+
 const createRoom = (firstPlayer) => {
   const roomId = uuid();
   const board = createBoard();
@@ -13,4 +15,9 @@ const createRoom = (firstPlayer) => {
   };
 };
 
-export { createRoom };
+const searchRoom = (roomID) => {
+  const target = rooms.find((room) => room.roomId === roomID);
+  return target;
+};
+
+export { rooms, createRoom, searchRoom };
