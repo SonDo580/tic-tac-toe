0bdf2b59f992893de175346081016b94d2bce1a8
Son Do <dominhson580@gmail.com>
2023-10-10 16:21:29 +0700

    Extract room controller

---
+++ server/controllers/room.js
@@ -0,0 +1,47 @@
+import { createRoom, rooms } from "../utils/room";
+
+const createRoomHandler = (socket) => (playerName) => {
+  if (playerName.trim() === "") {
+    socket.emit("nameError");
+    return;
+  }
+
+  const firstPlayer = { playerId: socket.id, playerName, mark: MARK.X };
+  const room = createRoom(firstPlayer);
+  rooms.push(room);
+  socket.emit("roomCreated", room);
+};
+
+const joinRoomHandler =
+  ({ socket, io }) =>
+  ({ playerName, roomID }) => {
+    if (playerName.trim() === "") {
+      socket.emit("nameError");
+      return;
+    }
+
+    if (roomID === "") {
+      socket.emit("roomIdEmpty");
+      return;
+    }
+
+    const room = searchRoom(roomID);
+    if (!room) {
+      socket.emit("roomNotExists");
+      return;
+    }
+
+    if (room.players.length === 2) {
+      socket.emit("roomFull");
+      return;
+    }
+
+    const secondPlayer = { playerId: socket.id, playerName, mark: MARK.O };
+    room.players.push(secondPlayer);
+    socket.emit("roomJoined", room);
+
+    const firstPlayerId = room.players[0].playerId;
+    io.to(firstPlayerId).emit("roomJoined", room);
+  };
+
+export { createRoomHandler, joinRoomHandler };
--- server/socket.js
+++ server/socket.js
@@ -1,7 +1,5 @@
 import { Server } from "socket.io";
-
-import { MARK } from "./constants.js";
-import { createRoom, rooms, searchRoom } from "./utils/room.js";
+import { createRoomHandler, joinRoomHandler } from "./controllers/room.js";
 
 const runSocketIO = (httpServer) => {
   const allowOrigins = ["http://localhost:5173"]; // add live client URL later
@@ -10,49 +8,8 @@ const runSocketIO = (httpServer) => {
   });
 
   io.on("connection", (socket) => {
-    const playerId = socket.id;
-
-    socket.on("createRoom", (playerName) => {
-      if (playerName.trim() === "") {
-        socket.emit("nameError");
-        return;
-      }
-
-      const firstPlayer = { playerId, playerName, mark: MARK.X };
-      const room = createRoom(firstPlayer);
-      rooms.push(room);
-      socket.emit("roomCreated", room);
-    });
-
-    socket.on("joinRoom", ({ playerName, roomID }) => {
-      if (playerName.trim() === "") {
-        socket.emit("nameError");
-        return;
-      }
-
-      if (roomID === "") {
-        socket.emit("roomIdEmpty");
-        return;
-      }
-
-      const room = searchRoom(roomID);
-      if (!room) {
-        socket.emit("roomNotExists");
-        return;
-      }
-
-      if (room.players.length === 2) {
-        socket.emit("roomFull");
-        return;
-      }
-
-      const secondPlayer = { playerId, playerName, mark: MARK.O };
-      room.players.push(secondPlayer);
-      socket.emit("roomJoined", room);
-
-      const firstPlayerId = room.players[0].playerId;
-      io.to(firstPlayerId).emit("roomJoined", room);
-    });
+    socket.on("createRoom", createRoomHandler(socket));
+    socket.on("joinRoom", joinRoomHandler({ socket, io }));
   });
 };
 
