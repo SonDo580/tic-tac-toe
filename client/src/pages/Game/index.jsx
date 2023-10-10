c36df4a886d69a3cf2e06a0dcd81d5b5ee85c909
Son Do <dominhson580@gmail.com>
2023-10-10 11:14:13 +0700

    Display room ID and player info

--- client/src/pages/Game/index.jsx
+++ client/src/pages/Game/index.jsx
@@ -2,19 +2,25 @@ import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 import { GameContext } from "@/context/GameContext";
+import { getPlayerRoles, getPlayerDisplay } from "@/utils/player";
 import Board from "@/components/Board";
 
 export default function Game() {
   const {
-    state: { roomId },
+    state: { roomId, players },
   } = useContext(GameContext);
 
   if (!roomId) {
     return <Navigate to="/" />;
   }
 
+  const { thisPlayer, otherPlayer } = getPlayerRoles(players);
+
   return (
     <div>
+      <h1>Room ID: {roomId}</h1>
+      <p>You: {getPlayerDisplay(thisPlayer)}</p>
+      <p>Opponent: {getPlayerDisplay(otherPlayer)}</p>
       <Board />
     </div>
   );
