988bef8edaf5c617dbc66ae4fcd40ceea9598756
Son Do <dominhson580@gmail.com>
2023-10-10 09:41:10 +0700

    Init game and navigate to Game page

--- client/src/pages/CreateRoom/index.jsx
+++ client/src/pages/CreateRoom/index.jsx
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { socket } from "@/utils/socket";
+import { GameContext } from "@/context/GameContext";
 
 export default function CreateRoom() {
+  const navigate = useNavigate();
+  const { initGame } = useContext(GameContext);
   const [name, setName] = useState("");
 
   const changeName = (event) => {
@@ -12,6 +17,16 @@ export default function CreateRoom() {
     event.preventDefault();
     socket.emit("createRoom", name);
   };
+
+  useEffect(() => {
+    socket.on("roomCreated", (roomInfo) => {
+      initGame(roomInfo);
+      navigate({
+        pathname: "/game",
+        search: `?room=${roomInfo.roomId}`,
+      });
+    });
+  }, []);
 
   return (
     <form onSubmit={handleSubmit}>
