6b7ff8392e1148e23341bdba55e87cc01cda8eff
Son Do <dominhson580@gmail.com>
2023-10-16 11:36:24 +0700

    Handle rematch on server side

--- server/socket.js
+++ server/socket.js
@@ -7,6 +7,8 @@ import {
   leaveRoomHandler,
 } from "./controllers/room.js";
 import { moveHandler } from "./controllers/game.js";
+import { resetRoom, searchRoomById } from "./utils/room.js";
+import { swapMark } from "./utils/mark.js";
 
 const runSocketIO = (httpServer) => {
   const allowedOrigins = ["http://localhost:5173"]; // add live client URL later
@@ -21,6 +23,24 @@ const runSocketIO = (httpServer) => {
     socket.on("disconnect", disconnectHandler({ socket, io }));
 
     socket.on("move", moveHandler({ socket, io }));
+    socket.on("rematch", (roomId) => {
+      // Find the room
+      const room = searchRoomById(roomId);
+      if (!room) {
+        return;
+      }
+
+      // The second event will not trigger this code
+      if (room.endGame) {
+        // Swap the players' marks
+        swapMark(room.players);
+        // Reset room state
+        resetRoom(room);
+      }
+
+      // Notice this player
+      socket.emit("rematched", room);
+    });
   });
 };
 
