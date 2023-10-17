d3fc7b960144e9f46f4f55ed716753ef897a88e3
Son Do <dominhson580@gmail.com>
2023-10-17 21:12:35 +0700

    Add ToastContainer and style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./App.scss";
 import Starter from "@/pages/Starter";
@@ -8,12 +10,15 @@ import JoinRoom from "@/pages/JoinRoom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Starter />} />
-      <Route path="/create" element={<CreateRoom />} />
-      <Route path="/join" element={<JoinRoom />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Starter />} />
+        <Route path="/create" element={<CreateRoom />} />
+        <Route path="/join" element={<JoinRoom />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
