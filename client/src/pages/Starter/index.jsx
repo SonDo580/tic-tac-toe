0020d5906877ac99f513b917d3045c3814a0b0ae
Son Do <dominhson580@gmail.com>
2023-10-17 12:12:03 +0700

    Style the Starter page

--- client/src/pages/Starter/index.jsx
+++ client/src/pages/Starter/index.jsx
@@ -2,10 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Starter() {
   return (
-    <div>
+    <div className="starter">
       <h1>Welcome to Tic-Tac-Toe</h1>
-      <Link to="create">Create room</Link>
-      <Link to="join">Join a room</Link>
+      <Link to="create" className="button">
+        Create room
+      </Link>
+      <Link to="join" className="button">
+        Join a room
+      </Link>
     </div>
   );
 }
