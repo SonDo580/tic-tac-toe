323d9bc70d53a1d7ed7e2de68b91d52c79d93a71
Son Do <dominhson580@gmail.com>
2023-10-18 17:10:47 +0700

    Handle reset current match on server side

--- server/socket.js
+++ server/socket.js
@@ -7,6 +7,7 @@ import {
   leaveRoomHandler,
 } from "./controllers/room.js";
 import { moveHandler, rematchHandler } from "./controllers/game.js";
+import { findPlayer, resetRoom, searchRoomById } from "./utils/room.js";
 
 const runSocketIO = (httpServer) => {
   const allowedOrigins = [
@@ -26,6 +27,79 @@ const runSocketIO = (httpServer) => {
 
     socket.on("move", moveHandler({ socket, io }));
     socket.on("rematch", rematchHandler(socket));
+
+    socket.on("resetRequest", (roomId) => {
+      // Find the room
+      const room = searchRoomById(roomId);
+      if (!room) {
+        return;
+      }
+
+      // Find players in the room
+      const { thisPlayer, otherPlayer } = findPlayer({
+        room,
+        playerId: socket.id,
+      });
+      if (!thisPlayer) {
+        return;
+      }
+
+      // Allow reset if there's only 1 player
+      if (!otherPlayer) {
+        // Reset room state
+        resetRoom(room);
+        // Notice current player
+        socket.emit("resetAccepted", room);
+        return;
+      }
+
+      // Ask for the other player's acceptance
+      io.to(otherPlayer.playerId).emit("resetRequest");
+    });
+
+    socket.on("resetAccept", (roomId) => {
+      // Find the room
+      const room = searchRoomById(roomId);
+      if (!room) {
+        return;
+      }
+
+      // Find players in the room
+      const { thisPlayer, otherPlayer } = findPlayer({
+        room,
+        playerId: socket.id,
+      });
+      if (!thisPlayer || !otherPlayer) {
+        return;
+      }
+
+      // Reset room state
+      resetRoom(room);
+
+      // Notify both players
+      socket.emit("resetAccepted", room);
+      io.to(otherPlayer.playerId).emit("resetAccepted", room);
+    });
+
+    socket.on("resetReject", (roomId) => {
+      // Find the room
+      const room = searchRoomById(roomId);
+      if (!room) {
+        return;
+      }
+
+      // Find players in the room
+      const { thisPlayer, otherPlayer } = findPlayer({
+        room,
+        playerId: socket.id,
+      });
+      if (!thisPlayer || !otherPlayer) {
+        return;
+      }
+
+      // Notify the other player
+      io.to(otherPlayer.playerId).emit("resetRejected");
+    });
   });
 };
 
