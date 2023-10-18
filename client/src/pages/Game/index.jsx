d0c5d4f9cb998b9c865732ebc2faa6bc40547ecc
Son Do <dominhson580@gmail.com>
2023-10-18 17:10:20 +0700

    Handle accept and reject reset request

--- client/src/pages/Game/index.jsx
+++ client/src/pages/Game/index.jsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import { MESSAGE } from "@/constants/messages";
 import { socket } from "@/utils/socket";
@@ -12,6 +13,7 @@ import RoomInfo from "@/components/RoomInfo";
 export default function Game() {
   const navigate = useNavigate();
   const { roomId, players, turn, endGame, winnerId } = useContext(GameContext);
+  const [resetPopupVisible, setResetPopupVisible] = useState(false);
 
   const leaveRoom = () => {
     socket.emit("leaveRoom", roomId);
@@ -20,6 +22,26 @@ export default function Game() {
   const rematch = () => {
     socket.emit("rematch", roomId);
   };
+
+  const acceptReset = () => {
+    socket.emit("resetAccept", roomId);
+    setResetPopupVisible(false);
+  };
+
+  const rejectReset = () => {
+    socket.emit("resetReject", roomId);
+    setResetPopupVisible(false);
+  };
+
+  useEffect(() => {
+    socket.on("resetRequest", () => {
+      setResetPopupVisible(true);
+    });
+
+    socket.on("resetRejected", () => {
+      toast(MESSAGE.resetRejected);
+    });
+  }, []);
 
   useEffect(() => {
     if (!roomId) {
@@ -44,6 +66,14 @@ export default function Game() {
       />
 
       <Board allowMove={allowMove} />
+
+      {resetPopupVisible && (
+        <Confirm
+          question={MESSAGE.resetQuestion}
+          onOk={acceptReset}
+          onCancel={rejectReset}
+        />
+      )}
 
       {endGame && (
         <Confirm
