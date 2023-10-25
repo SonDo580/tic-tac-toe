4c24fc9525901bcc607465cd952ac9c463d75ce5
Son Do <dominhson580@gmail.com>
2023-10-25 22:40:47 +0700

    Don't pass object

--- server/socket.ts
+++ server/socket.ts
@@ -1,4 +1,4 @@
-import type { Server as HttpServer } from "http";
+import { Server as HttpServer } from "http";
 import { Server } from "socket.io";
 
 import {
@@ -26,14 +26,14 @@ const runSocketIO = (httpServer: HttpServer) => {
 
   io.on("connection", (socket) => {
     socket.on("createRoom", createRoomHandler(socket));
-    socket.on("joinRoom", joinRoomHandler({ socket, io }));
-    socket.on("leaveRoom", leaveRoomHandler({ socket, io }));
-    socket.on("disconnect", disconnectHandler({ socket, io }));
-    socket.on("move", moveHandler({ socket, io }));
+    socket.on("joinRoom", joinRoomHandler(socket, io));
+    socket.on("leaveRoom", leaveRoomHandler(socket, io));
+    socket.on("disconnect", disconnectHandler(socket, io));
+    socket.on("move", moveHandler(socket, io));
     socket.on("rematch", rematchHandler(socket));
-    socket.on("resetRequest", resetRejectHandler({ socket, io }));
-    socket.on("resetAccept", resetAcceptHandler({ socket, io }));
-    socket.on("resetReject", resetRejectHandler({ socket, io }));
+    socket.on("resetRequest", resetRejectHandler(socket, io));
+    socket.on("resetAccept", resetAcceptHandler(socket, io));
+    socket.on("resetReject", resetRejectHandler(socket, io));
   });
 };
 
