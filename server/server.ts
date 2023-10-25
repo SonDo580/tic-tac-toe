d9d4f61b556fbfb08158a5b17ade8d9f2088fe8a
Son Do <dominhson580@gmail.com>
2023-10-25 21:55:38 +0700

    Convert server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { createServer } from "node:http";
-import runSocketIO from "./socket.js";
+import { createServer } from "http";
+
+import runSocketIO from "./socket";
 
 const app = express();
 const server = createServer(app);
