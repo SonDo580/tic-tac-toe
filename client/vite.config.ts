876775270a315f3fd223c9a5114917c84f236dec
Son Do <dominhson580@gmail.com>
2023-10-26 10:29:41 +0700

    Config for absolute import

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import tsconfigPaths from "vite-plugin-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.join(__dirname, "src"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
