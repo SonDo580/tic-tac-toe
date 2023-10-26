96de600897abd60665d5447f14b1b197d8ecf74f
Son Do <dominhson580@gmail.com>
2023-10-26 12:06:17 +0700

    Change import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "@/App.tsx";
+import App from "./App.tsx";
 import { GameProvider } from "@/context/GameContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
