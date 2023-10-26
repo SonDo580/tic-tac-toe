c2338381887a684c05219e1e9dbb24072b862db8
Son Do <dominhson580@gmail.com>
2023-10-26 10:34:51 +0700

    Use absolute import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App.tsx";
-import { GameProvider } from "./context/GameContext.tsx";
+import App from "@/App.tsx";
+import { GameProvider } from "@/context/GameContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
